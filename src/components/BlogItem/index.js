7f1543964ead7e2eaba11fd1d4beef819c7867a1
saiprakashgundemeda <gundemedasaiprakashgmail.com>
2024-03-07 10:50:07 +0000

    Blog-List

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,21 @@
+// Write your JS code here
+
+import './index.css'
+
+const BlogItem = props => {
+  const {item} = props
+  return (
+    <div className="blog-list-items-container">
+      <div className="sub-items">
+        <h1 className="title">{item.title}</h1>
+        <p className="date">{item.publishedDate}</p>
+      </div>
+      <div>
+        <p className="desc">{item.description}</p>
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default BlogItem
